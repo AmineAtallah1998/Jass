3fafe729dd955cdd93b6d1e498f750ea4593b037
Dhraief <mohamed.dhraief@epfl.ch>
2019-04-10 13:25:07 +0200

    AASBA

--- Jass/src/ch/epfl/javass/Preconditions.java
+++ Jass/src/ch/epfl/javass/Preconditions.java
@@ -1,9 +1,16 @@
 package ch.epfl.javass;
 
+/**
+ * @author Mohamed Ali Dhraief & Amine Atallah 
+ *
+ */
 public final class Preconditions {
     
     private Preconditions() {}
     
+    /**
+     * @param b
+     */
     public static void checkArgument(boolean b) {
         if(b==false) {
             throw new IllegalArgumentException();
@@ -11,6 +18,12 @@ public final class Preconditions {
         
     }
     
+    
+    /**
+     * @param index
+     * @param size
+     * @return
+     */
     public static int checkIndex(int index , int size) {
         if(index<0 || index>=size) {
             throw new IndexOutOfBoundsException();
